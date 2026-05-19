a05e0f4d14547f231583f495e51a230b97046d5f
ka9mal6t <vladimyr.kilko@gmail.com>
2026-05-19 12:21:39 +0200

    add delete function

--- admin/words.py
+++ admin/words.py
@@ -18,14 +18,32 @@ class WordAdminDAO:
             tuple: A boolean indicating success and a message
         """
         if self.db.get_word_by_name(word):
-            Log.info(f"Attempt to add existing word: {word} | {level_name}")
+            Log.warning(f"Attempt to add existing word: {word} | {level_name}")
             return False, f"Word '{word}' already exists."
         
         level = self.db.get_level_by_name(level_name)
         if not level or level_name not in Level.__members__:
-            Log.info(f"Attempt to add word with non-existent level: {level_name}")
+            Log.warning(f"Attempt to add word with non-existent level: {level_name}")
             return False, f"Level '{level_name}' does not exist."
         
         self.db.add_word(word, level.id)
         Log.info(f"Word added successfully: {word} | {level_name}")
         return True, f"Word '{word}' added successfully."
+    
+    def delete_word(self, word):
+        """
+        Delete a word from the database
+        Args:
+            word (str): The word to delete
+        Returns:
+            tuple: A boolean indicating success and a message
+        """
+
+        word_obj = self.db.get_word_by_name(word)
+        if not word_obj:
+            Log.warning(f"Attempt to delete non-existent word: {word}")
+            return False, f"Word '{word}' does not exist."
+        
+        self.db.delete_word(word_obj.id)
+        Log.info(f"Word deleted successfully: {word} | {Level(word_obj.level_id).name}")
+        return True, f"Word '{word}' deleted successfully."
